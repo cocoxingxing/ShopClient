66d40127eb75e3a1037b39bc00d467879db1526c
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 00:54:52 +0800

    1.implement main function in the Main.java

--- src/main/java/tju/edu/coco/main/Main.java
+++ src/main/java/tju/edu/coco/main/Main.java
@@ -4,7 +4,17 @@
 
 package tju.edu.coco.main;
 
+import java.util.Hashtable;
+
+import tju.edu.coco.consts.Consts;
+import tju.edu.coco.utils.FileHelper;
+
 public class Main {
+    public static ShopClient shopClient = new ShopClient();
     public static void main(String[] args) {
+        Hashtable<String, Integer> shoppingItems = FileHelper.readFileToItems(Consts.ITEM_FILE_PATH);
+        shopClient.setShoppingItems(shoppingItems);
+        shopClient.settlement();
+        shopClient.printShoppingList();
     }
 }
