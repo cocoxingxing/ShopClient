7eae61f99f44a1f2f8729786909d9e4749ba9718
kehanliu <kehanliu@tju.edu.cn>
2016-07-19 00:52:03 +0800

    1.implement the ShopClient constructure;2.implement the function discountItem function in ShopClient for computing the shopping item;3.implement the settlement function in ShopClient for computing the final shopping list

--- src/main/java/tju/edu/coco/main/ShopClient.java
+++ src/main/java/tju/edu/coco/main/ShopClient.java
@@ -16,6 +16,7 @@ public class ShopClient {
     private Set<String> discount95 = new HashSet<String>();
     private Hashtable<String, Item> dbItems = new Hashtable<String, Item>();
     private Hashtable<String, Integer> shoppingItems = new Hashtable<String, Integer>();
+    private Set<ShoppingItem> finalShoppingItems = new LinkedHashSet<ShoppingItem>();
 
     public ShopClient() {
         loadConfigInfo();
@@ -49,6 +50,51 @@ public class ShopClient {
      * settlement for the shopping items
      */
     public void settlement() {
+        Enumeration<String> keys = this.shoppingItems.keys();
+        while(keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            ShoppingItem item = discountItem(key, this.shoppingItems.get(key));
+            if(item != null) {
+                finalShoppingItems.add(item);
+            }
+        }
+    }
 
+    /**
+     * find the item discount info and count the final ShoppingItem
+     * @param code
+     * @param num
+     * @return
+     */
+    public ShoppingItem discountItem(String code, int num) {
+        ShoppingItem shoppingItem = new ShoppingItem();
+        Enumeration<String> keys = this.dbItems.keys();
+
+        Item item = null;
+        if(dbItems.containsKey(code)) {
+            item = dbItems.get(code);
+        } else {
+            return null;
+        }
+        shoppingItem.setName(item.getName());
+        shoppingItem.setPrice(item.getPrice());
+        shoppingItem.setNum(num);
+        shoppingItem.setTotalPrice(Tools.formatDouble(num*item.getPrice()));
+        String tag = "";
+        if(discountThreeForTwo.contains(code) && num >= 3) {
+            tag = "3for2";
+            int discountNum = Math.floorDiv(num, 3);
+            shoppingItem.setDiscountPrice(Tools.formatDouble(item.getPrice() * discountNum));
+        }
+        if(tag.equals("") && discount95.contains(code)) {
+            tag = "95%";
+            shoppingItem.setDiscountPrice(Tools.formatDouble(item.getPrice() * num * 0.05));
+        }
+        if(tag.equals("")) {
+            shoppingItem.setDiscountPrice(0.00);
+        }
+        shoppingItem.setTotalPrice(Tools.formatDouble(shoppingItem.getTotalPrice() - shoppingItem.getDiscountPrice()));
+        shoppingItem.setDiscountTag(tag);
+        return shoppingItem;
     }
 }
